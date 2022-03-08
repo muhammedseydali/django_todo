3c39b1db267a90fb6fafd5daa0a0bf72d15a2c08
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-03-08 17:36:34 +0530

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -122,8 +122,6 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATIC_URL = '/static/'
