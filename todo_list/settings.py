64a7b6c7052a0aba8d12b46905cc3869868aabaa
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-03-08 17:24:58 +0530

    s

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['to-doapplications.herokuapp.com', '127.0.0.1']
 
