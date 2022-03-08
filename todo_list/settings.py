05513404c2d71b97a78ffd57f6645cd8f0efa0e9
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-03-08 17:27:32 +0530

    s

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -124,7 +124,6 @@ LOGIN_URL = 'login'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = BASE_DIR/ 'static'
+
 
